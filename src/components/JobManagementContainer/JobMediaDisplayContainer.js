3dc8749559eade4d84aaae1095ef07e37afe7789
Daniel Fontaine <fontaine@sfsu.edu>
2020-09-21 14:40:47 -0700

    ALT_MEDIA_SERVICES-21 Set Source URL CSS to text cutoff

--- src/components/JobManagementContainer/JobMediaDisplayContainer.js
+++ src/components/JobManagementContainer/JobMediaDisplayContainer.js
@@ -66,8 +66,8 @@ class JobMediaDisplayContainer extends Component {
                                 {this.props.media.media_type === 'File' && (<label style={{'margin-right': '32px'}}>Source File: </label>)}
                                 {this.props.media.media_type === 'File' && <a href={this.props.download_url}>{this.props.fileObject.associated_files.file_name}</a>}
 
-                                {this.props.media.media_type === 'URL' && (<label style={{'margin-right': '32px'}}>Source URL: </label>)}
-                                {this.props.media.media_type === 'URL' && (<div><a target="_blank" href={this.state.source_url}>{this.state.source_url}</a></div>)}
+                                {this.props.media.media_type === 'URL' && (<div><label style={{'margin-right': '32px'}}>Source URL: </label></div>)}
+                                {this.props.media.media_type === 'URL' && (<div className={"sourceUrlLink"}><a target="_blank" href={this.state.source_url}>{this.state.source_url}</a></div>)}
                             </div>
 
                             <div className={"extractorButtonsContainer"}>
