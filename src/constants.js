ea3504ee78bdc6438399c98f5e442a06da519f96
Daniel Fontaine <fontaine@sfsu.edu>
2021-02-16 17:17:04 -0800

    Test new server

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export const devServerApiURL = () => ('http://localhost:5000/api/v2/captioning')
-export const deployServerApiURL = () => ('https://amp-dev.sfsu.edu//api/v2/captioning')
+export const deployServerApiURL = () => ('http://ec2-34-216-255-37.us-west-2.compute.amazonaws.com/api/v2/captioning')
 export const iLearnURL = () => ('https://ilearn.support.at.sfsu.edu/ay2021/course/view.php?id=')
 export const fileDownloadUrl = () => ("http://localhost:5000/api/v2/captioning/services/download/file")
 export const astJobURL = () =>  ("https://web.automaticsync.com/show_details.php?show_id=")
