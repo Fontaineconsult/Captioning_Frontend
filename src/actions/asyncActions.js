660016a8d2189e41f58fcf60178afaf5ac12deb1
A-Singh15 <asingh39@sfsu.edu>
2024-03-13 13:48:48 -0700

    updated newJobMasterContainer.js as well as asyncAction.js for greyout functions

---
+++ src/actions/asyncActions.js
@@ -0,0 +1,10 @@
+// asyncActions.js
+
+// Define action types
+export const JOB_SUBMISSION_COMPLETED = 'JOB_SUBMISSION_COMPLETED';
+export const CLEAR_MEDIA = 'CLEAR_MEDIA';
+export const CLEAR_MEDIA_COMPLETED = 'CLEAR_MEDIA COMPLETED';
+
+
+
+
