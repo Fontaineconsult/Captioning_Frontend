7c1f26273954c876badebee19bcbed24323dc51c
A-Singh15 <asingh39@sfsu.edu>
2024-07-02 08:52:47 -0700

    feat: Make Employee ID, First Name, Last Name, and Email required in Add Employee form
    - Added `required` attribute to Employee ID, First Name, Last Name, and Email input fields in the Add Employee form.
    - Ensured proper validation for these required fields to improve form submission accuracy.

--- src/components/UsersManagementContainer/Employees/addEmployeeContainer.js
+++ src/components/UsersManagementContainer/Employees/addEmployeeContainer.js
@@ -1,11 +1,9 @@
 import React, {Component} from 'react';
 import {withRouter} from "react-router";
 import {connect} from "react-redux";
-
-import {AddEmployee} from '../../../actions/ampApi/postData'
+import {AddEmployee} from '../../../actions/ampApi/postData';
 
 class AddEmployeeContainer extends Component {
-
     constructor(props) {
         super(props);
         this.state = {
@@ -15,88 +13,71 @@ class AddEmployeeContainer extends Component {
             email: '',
             phoneNumber: '',
         };
-        this.handleChange = this.handleChange.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
-
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-
     handleChange(event) {
-        event.preventDefault()
-
+        event.preventDefault();
         this.setState({[event.target.name]: event.target.value});
     }
 
     handleSubmit(event) {
-        event.preventDefault()
-
-        this.props.dispatch(AddEmployee(this.state))
+        event.preventDefault();
+        this.props.dispatch(AddEmployee(this.state));
         this.setState({
             employeeId: '',
             firstName: '',
             lastName: '',
             email: '',
             phoneNumber: ''
-        })
-
+        });
     }
 
     render() {
-
         return (
             <div>
                 <div className="addEmployeeContainer emp-top">
                     <form onSubmit={this.handleSubmit}>
                         <label htmlFor={"employeeId"}>
                             <div>Employee ID</div>
-                            <input className={"emp-input"} maxLength={9} value={this.state.employeeId} type={'text'}
+                            <input className={"emp-input"} required={true} maxLength={9} value={this.state.employeeId} type={'text'}
                                    name={"employeeId"} onChange={this.handleChange}/>
                         </label>
-
                         <label htmlFor={"firstName"}>
                             <div>First Name</div>
                             <input className={"emp-input"} required={true} value={this.state.firstName} type={'text'}
                                    name={"firstName"} onChange={this.handleChange}/>
                         </label>
-
-
                         <label htmlFor={"lastName"}>
                             <div>Last Name</div>
                             <input className={"emp-input"} required={true} value={this.state.lastName} type={'text'}
                                    name={"lastName"} onChange={this.handleChange}/>
                         </label>
-
                         <label htmlFor={"email"}>
                             <div>E-Mail</div>
-                            <input className={"emp-input"} required={true} value={this.state.email} type={'text'}
+                            <input className={"emp-input"} required={true} value={this.state.email} type={'email'}
                                    name={"email"} onChange={this.handleChange}/>
                         </label>
                         <label htmlFor={"phoneNumber"}>
                             <div>Phone Number</div>
                             <input className={"emp-input"} value={this.state.phoneNumber} type={'text'}
-                                   name={"phoneNumber"}
-                                   onChange={this.handleChange}/>
+                                   name={"phoneNumber"} onChange={this.handleChange}/>
                         </label>
                         <div>
                             <input type="submit" name="Submit"/>
                         </div>
                     </form>
-
                 </div>
             </div>
-
-        )
-
+        );
     }
-
-
 }
-
 
 function mapStateToProps({videosJobsReducer}, {props}) {
-
-
-    return {}
+    return {};
 }
 
-export default withRouter(connect(mapStateToProps)(AddEmployeeContainer))
+export default withRouter(connect(mapStateToProps)(AddEmployeeContainer));
+
+
